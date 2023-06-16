f2a80ed475136ecfed6dc0dde7e54a25cce3113d
AdrianBulla05 <abullabe@ibero.edu.co>
2023-06-16 16:48:36 -0500

    Se crea un ejercicio de login en Angular con la funcionalidad de editar perfiles y crear nuevos usuarios

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+//Componentes del login
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+
+}
